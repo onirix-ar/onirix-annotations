e5725ce76c3e660a52fda77ec88e50644ebada18
Laura Fernández García <laura.fernandez@onirix.com>
2024-04-11 10:31:15 +0200

    ENG-1480 fix pr

--- src/constants/default.css.js
+++ src/constants/default.css.js
@@ -204,7 +204,7 @@ const DEFAULT_CSS = `
     position: absolute;
     top: 0;
     width: 100%;
-    height: 100%;
+    height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
